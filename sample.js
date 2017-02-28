c463e704c9a2720a84b6d635c03566c9226f6138
BhanuPatnana <bpatnana@miraclesoft.com>
2017-02-28 23:15:33 +0530

    Sample.js file is commited

---
+++ sample.js
@@ -0,0 +1,62 @@
+
+//  Build our app module, with a dependency on the angular modal service.
+var app = angular.module('sampleapp', ['angularModalService', 'ngAnimate']);
+
+app.controller('SampleController', ['$scope', 'ModalService', function($scope, ModalService) {
+
+  $scope.complexResult = null;
+
+  $scope.showComplex = function() {
+
+    ModalService.showModal({
+      templateUrl: "modal.html",
+      controller: "ComplexController",
+      inputs: {
+        title: "Provide Your Details"
+      }
+    }).then(function(modal) {
+      modal.element.modal();
+      modal.close.then(function(result) {
+        $scope.complexResult  = "Name: " + result.name + ", age: " + result.age;
+      });
+    });
+
+  };
+
+  
+}]);
+
+//model controller
+
+app.controller('ComplexController', [
+  '$scope', '$element', 'title', 'close', 
+  function($scope, $element, title, close) {
+
+  $scope.name = null;
+  $scope.age = null;
+  $scope.title = title;
+  
+  //  This close function doesn't need to use jQuery or bootstrap, because
+  //  the button has the 'data-dismiss' attribute.
+  $scope.close = function() {
+ 	  close({
+      name: $scope.name,
+      age: $scope.age
+    }, 500); // close, but give 500ms for bootstrap to animate
+  };
+
+  //  This cancel function must use the bootstrap, 'modal' function because
+  //  the doesn't have the 'data-dismiss' attribute.
+  $scope.cancel = function() {
+
+    //  Manually hide the modal.
+    $element.modal('hide');
+    
+    //  Now call close, returning control to the caller.
+    close({
+      name: $scope.name,
+      age: $scope.age
+    }, 500); // close, but give 500ms for bootstrap to animate
+  };
+
+}]);
